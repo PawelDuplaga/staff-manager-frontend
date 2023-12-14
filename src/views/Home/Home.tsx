814854007493e59f7c0b80881be841709188b3a3
PawelDuplaga <pawelduplagait@gmail.com>
2023-12-14 09:00:46 +0100

    + Home View

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,10 @@
+import ManagerTopBar from '../../components/ManagerTopBar/ManagerTopBar';
 import styles from './home.module.scss';
 
 const Home = () => {
   return (
 		<div className={styles.homeContainer}>
-
+      <ManagerTopBar />
 		</div>
   );
 };
